d43834f502c911edc8c1c60e5b6ef0408c27ca71
furikake6000 <ewave63@keio.jp>
2019-10-21 00:52:10 +0900

    フォントのインストールとコンポーネントの追加

---
+++ src/components/Timer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="timer">
+    12:00
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Timer'
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  @font-face {
+    font-family: 'Memoir';
+    src: url('../assets/memoir-square.otf') format('truetype');
+  }
+
+  #timer {
+    font-family: 'Memoir';
+    font-size: 10rem;
+  }
+</style>
