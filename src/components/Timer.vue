15c3348e7110d82b73a31cdd3f6feb8d8719a85d
furikake6000 <ewave63@keio.jp>
2019-10-21 17:32:18 +0900

    node-sassの使用とsassへの書き換え

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -64,14 +64,12 @@
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-  @font-face {
-    font-family: 'Memoir';
-    src: url('../assets/memoir-square.otf') format('truetype');
-  }
+<style scoped lang='sass'>
+  @font-face
+    font-family: 'Memoir'
+    src: url('../assets/memoir-square.otf') format('truetype')
 
-  #timer {
-    font-family: 'Memoir';
-    font-size: 10rem;
-  }
+  #timer
+    font-family: 'Memoir'
+    font-size: 10rem
 </style>
