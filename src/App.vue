9bd0d32a63a3afd2c34414f2c9a6d9c1d8855138
furikake6000 <ewave63@keio.jp>
2019-11-04 16:26:31 +0900

    bulmaの導入

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
     <Timer />
   </div>
 </template>
@@ -15,7 +15,9 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import "../node_modules/bulma/bulma.sass";
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
