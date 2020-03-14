5466e910c71a85f0c12ed7892070c971a844bc3c
furikake6000 <ewave63@keio.jp>
2020-03-15 01:00:18 +0900

    既に共通定義されていたh1 cssを削除

--- src/components/Tasks/Create.vue
+++ src/components/Tasks/Create.vue
@@ -72,13 +72,6 @@ export default {
 </script>
 
 <style scoped lang='sass'>
-h1
-  display: block
-  color: #4CC729
-  background-color: #C2EFB5
-  margin: 0
-  padding: 2.5rem 0 1rem 0
-
 .task-create-form
   margin: 4rem 1rem 6rem 1rem
   font-weight: bold
--- src/components/Tasks/Index.vue
+++ src/components/Tasks/Index.vue
@@ -53,13 +53,6 @@ export default {
 </script>
 
 <style scoped lang='sass'>
-h1
-  display: block
-  color: #4CC729
-  background-color: #C2EFB5
-  margin: 0
-  padding: 2.5rem 0 1rem 0
-
 // タスク一覧
 .tasks
   margin:
